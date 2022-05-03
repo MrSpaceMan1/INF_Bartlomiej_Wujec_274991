aa1dd5fcc66da9d6ba58d9384b589c95957bc0ac
Bartłomiej Wujec <b.wujec.444@studms.ug.edu.pl>
2022-05-03 23:12:58 +0200

    a

--- Lab06/zad1.py
+++ Lab06/zad1.py
@@ -1,15 +1,61 @@
 import numpy as np
+import pandas as pd
 
-db = np.genfromtxt("./iris_with_errors.csv",
-                   encoding="utf",
-                   delimiter=',',
-                   names=['slength', 'swidth', 'plength', 'pwidth', 'variety'],
-                   dtype=np.dtype([('slength', 'float'),
-                                   ('swidth', 'float'),
-                                   ('plength', 'float'),
-                                   ('pwidth', 'float'),
-                                   ('variety', 'S20')]))
-db = np.array(db.tolist())
-print(db)
-db[:, 4] = [db[i, 4].lower() for i in range(db.shape[0])]
-print(db)
+df = pd.read_csv('./iris_with_errors.csv',
+                 dtype={'sepal.length': float,
+                        'sepal.width': float,
+                        'petal.length': float,
+                        'petal.width': float,
+                        'variety': str},
+                 na_values=['-', ' '])
+
+print(df.isnull().sum())
+print()
+
+medians_df = df.median(0, skipna=True, numeric_only=True)
+
+for name in ['sepal.length', 'sepal.width', 'petal.length', 'petal.width']:
+    index = 0
+    for i in df[name]:
+        try:
+            i = float(i)
+            if 15 >= i <= 0:
+                raise ValueError
+        except ValueError:
+            df.loc[index, name] = np.nan
+        index += 1
+
+index = 0
+for i in df["variety"]:
+    try:
+        str(i)
+    except ValueError:
+        df.loc[index, "variety"] = np.nan
+    index += 1
+
+for name in ['sepal.length', 'sepal.width', 'petal.length', 'petal.width']:
+    index = 0
+    for i in df.isnull()[name]:
+        if i:
+            df.loc[index, name] = medians_df[name]
+        index += 1
+
+print(df.isnull().sum())
+
+x = {
+    'vir': 'virginica',
+    'set': 'setosa',
+    'ver': 'versicolor'
+}
+
+for i in range(150):
+    df.loc[i, 'variety'] = df['variety'][i].lower()
+    if df.loc[i, 'variety'] not in ['virginica', 'setosa', 'versicolor']:
+        beginning = df['variety'][i][:3]
+        try:
+            df.loc[i, 'variety'] = x[beginning]
+        except KeyError:
+            df.drop(axis=0, index=i)
+
+
+print(df)
